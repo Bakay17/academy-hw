e70fe6b65a252a5af902a19edb1242d70c3d175c
Bakay17 <bakayberdibekov@gmail.com>
2021-08-15 05:28:13 +0300

    homework 46

---
+++ src/com/company/Month.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    MART,
+    APRIL,
+    MAY,
+    JUNE,
+    JULE,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER;
+}
