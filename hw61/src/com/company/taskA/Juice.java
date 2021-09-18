bf336de8ea10f025afcf1265e73b5312c212032b
Bakay17 <bakayberdibekov@gmail.com>
2021-09-18 16:36:43 +0300

    add hw 61 - GENERICS methods

---
+++ hw61/src/com/company/taskA/Juice.java
@@ -0,0 +1,8 @@
+package com.company.taskA;
+
+public class Juice implements Liquid{
+    @Override
+    public void pourOver() {
+        System.out.println("Я сок, и я переливаюсь");
+    }
+}
---
+++ hw61/src/com/company/taskA/Liquid.java
@@ -0,0 +1,5 @@
+package com.company.taskA;
+
+public interface Liquid {
+    void pourOver();
+}
